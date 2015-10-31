07502523b9641208585440e32523594d6ccc2df7
sophiaahn <sophiajhahn@gmail.com>
2015-10-31 16:17:48 +0900

    My first codecamp

---
+++ codeacademy_python_section_01.py
@@ -0,0 +1,61 @@
+print "Welcom to Python!"
+spam = 5
+my_variable = 10
+# 앞서 설명 란에서 얘기한 변수와 각 변수의 값들을 지정하세요!
+my_int = 7
+my_float = 1.23
+my_bool = True
+# 아래에 변수 my_int의 값은 7로 지정되어 있습니다.
+# 지정된 값을 3으로 변경하고 결과를 출력하려면 어떻게 해야할까요?
+
+my_int = 7
+
+# 여덟 번째 줄에서 변수 my_int의 값을 3 으로 변경하세요!
+
+my_int = 3
+
+# 변수 my_int의 값을 console에 출력하는 코드는 아래와 같습니다:
+# 여기서 사용된 print 키워드는 이후 더 자세히 다룰 예정입니다!
+
+print my_int
+def spam():
+    eggs = 12
+    return eggs
+
+print spam()
+def spam():
+    eggs = 12
+    return eggs
+
+print spam()
+spam = True
+eggs = False
+
+mysterious_variable = 42
+#파이썬 시작
+
+"""저는 나무꾼입니다.
+낮에는 일을 하고, 밤에는 잠을 잡니다!"""
+
+# 세 번째 줄에서 변수 count_to의 값으로 1 더하기 2를 지정하세요!
+
+count_to = 1+2
+
+print count_to
+
+# 세 번째 줄에서 거듭제곱을 이용하여 변수 eggs의 값을 100으로 지정하세요!
+
+eggs = 10 ** 2
+
+print eggs
+
+# 세 번째 줄에서 모듈로 연산을 이용하여 변수 spam의 값을 1로 지정하세요!
+
+spam = 5 % 2
+
+print spam
+
+# 파이썬 시작
+monty = True
+python = 1.234
+monty_python = python ** 2
